a03bbbfe39b8840a0fce734c8c4fc8cfd02b7655
Musthafa <mohdmusthafacbe@gmail.com>
2022-06-22 16:56:38 +0530

    Second

---
+++ node/Calculation.js
@@ -0,0 +1,8 @@
+var calculator=require('./Calculator')
+
+var a=10
+var b=20
+
+console.log("Addition :"+calculator.add(a,b))
+console.log("Subtraction :"+calculator.subtract(a,b))
+console.log("Multiplication :"+calculator.mutliply(a,b))
---
+++ node/Calculator.js
@@ -0,0 +1,11 @@
+exports.add =function(a,b){
+        return a+b
+}
+
+exports.subtract =function(a,b){
+    return a-b
+}
+
+exports.mutliply =function(a,b){
+    return a*b
+}
