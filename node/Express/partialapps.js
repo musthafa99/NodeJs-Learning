a0e71c33aa2192165ce8d58012e35318e03f4295
Musthafa <mohdmusthafacbe@gmail.com>
2022-06-24 15:29:40 +0530

    Express Template Engines

--- node/Express/partialapps.js
+++ node/Express/partialapps.js
@@ -4,18 +4,16 @@ app=express()
 app.set('view engine','ejs')
 
 app.get('/',function(req,res){
-    res.sendFile(__dirname+ '/home.html')
+    res.render('home')
 })
 
 app.get('/contact',function(req,res){
-    res.sendFile(__dirname+ '/contact.html')
+    res.render('contact')
 
 })
 
 app.get('/profile/:name',function(req,res){
     var data={age:29,job:'studying',hobbies:['eating','fishing','playing']}
-    res.render('profile',{person:req.params.name,data:data})
-
-
+    res.render('profile',{persons:req.params.name,datas:data})
 })
 app.listen(3000)
