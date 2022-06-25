ed696c753900eed7c3d4f1239fb0d84fb6e2ccf6
Musthafa <mohdmusthafacbe@gmail.com>
2022-06-25 10:31:07 +0530

    todo app

--- TodoAPP/Public/Assests/todolist.js
+++ TodoAPP/Public/Assests/todolist.js
@@ -1,69 +1,34 @@
-body{
-    background: #0d1521;
-    font-family: tahoma;
-    color: #989898;
-}
+$(document).ready(function(){
 
-#todo-table{
-    position: relative;
-    width: 95%;
-    background: #090d13;
-    margin: 0 auto;
-    padding: 20px;
-    box-sizing: border-box;
-}
-
-#todo-table form:after{
-    margin: 0;
-    content: '';
-    display: block;
-    clear: both;
-}
-
-input[type="text"]{
-    width: 70%;
-    padding: 20px;
-    background:#181c22;
-    border: 0;
-    float: left;
-    font-size: 20px;
-    color: #989898;
-}
-
-button{
-    padding: 20px;
-    width: 30%;
-    float: left;
-    background: #23282e;
-    border: 0;
-    box-sizing: border-box;
-    color: #fff;
-    cursor: pointer;
-    font-size: 20px;
-}
-
-ul{
-    list-style-type: none;
-    padding: 0;
-    margin: 0;
-}
-
-li{
-    width: 100%;
-    padding: 20px;
-    box-sizing: border-box;
-    font-family: arial;
-    font-size: 20px;
-    cursor: pointer;
-    letter-spacing: 1px;
-}
-
-li:hover{
-    text-decoration: line-through;
-    background: rgba(0,0,0,0.2);
-}
-
-h1{
-    margin-bottom: 0;
-    text-indent: -10000px;
-}
+    $('form').on('submit', function(){
+  
+        var item = $('form input');
+        var todo = {item: item.val()};
+  
+        $.ajax({
+          type: 'POST',
+          url: '/todo',
+          data: todo,
+          success: function(data){
+            //do something with the data via front-end framework
+            location.reload();
+          }
+        });
+  
+        return false;
+  
+    });
+  
+    $('li').on('click', function(){
+        var item = $(this).text().replace(/ /g, "-");
+        $.ajax({
+          type: 'DELETE',
+          url: '/todo/' + item,
+          success: function(data){
+            //do something with the data via front-end framework
+            location.reload();
+          }
+        });
+    });
+  
+  });
