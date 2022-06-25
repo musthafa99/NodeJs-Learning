c738dc07aebb2f6bcb2f35cb0098e0c0c93de36b
Musthafa <mohdmusthafacbe@gmail.com>
2022-06-25 13:07:41 +0530

    todo app

--- TodoAPP/Controllers/todocontrollers.js
+++ TodoAPP/Controllers/todocontrollers.js
@@ -1,13 +1,19 @@
-module.exports =function(app){
-
+var bodyparse=require('body-parser');
+var data=[{item:'walk'},{item:'eat'},{item:'code'}];
+module.exports=function(app){
+var urlencoderparser=bodyparse.urlencoded({extended:false});
 app.get('/todo',function(req,res){
-    res.render('todoview')
+    res.render('todoview',{value:data});
 });
-app.post('/todo',function(req,res){
-
+app.post('/todo',urlencoderparser,function(req,res){
+    data.push(req.body);
+    res.json(data);
 });
 
-app.delete('/todo',function(req,res){
-
+app.delete('/todo/:value',function(req,res){
+    data=data.filter(function(todo){
+        return todo.item.trim().replace(/ /g,'-') !==req.param.value;
+    });
+    res.json(data)
 });
 };
