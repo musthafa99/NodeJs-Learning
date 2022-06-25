907da298fce5402b947bb9ea51249edc9e366947
Musthafa <mohdmusthafacbe@gmail.com>
2022-06-25 17:20:55 +0530

    mongoose

--- Mongoose/app.js
+++ Mongoose/app.js
@@ -2,6 +2,18 @@ var express=require('express');
 var app=express();
 var mongoose=require('mongoose')
 var bodyparser=require('body-parser')
+var Book=require('./bookmodel')
 
+mongoose.connect('mongodb://localhost:27017/books')
+
+app.get('/',function(req,res){
+    res.send('welcome to books page')
+});
+
+app.get('/books',function(req,res){
+    Book.find({}, function(err,Book){
+        if(err) res.send(err);
+        res.json(Book);
+    });});
 app.listen(3000);
 console.log("Listening");
--- Mongoose/bookmodel.js
+++ Mongoose/bookmodel.js
@@ -1,1 +1,8 @@
-var 
+var mongoose=require('mongoose')
+var bookschema=new mongoose.Schema({
+    title:String,
+    author:String,
+    category:String
+});
+
+module.exports=mongoose.model('Books',bookschema)
