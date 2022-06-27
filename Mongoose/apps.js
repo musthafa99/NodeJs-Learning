6229e7098c7eaf037056ebf86f56f403a737bfe6
Musthafa <mohdmusthafacbe@gmail.com>
2022-06-27 11:27:18 +0530

    Crud Operations

--- Mongoose/apps.js
+++ Mongoose/apps.js
@@ -5,7 +5,7 @@ var bodyparser=require('body-parser')
 var Book=require('./bookmodel')
 
 mongoose.connect('mongodb://localhost:27017/books')
-
+var urlencoderparser=bodyparser.urlencoded({extended:false});
 app.get('/',function(req,res){
     res.send('welcome to books page')
 });
@@ -20,6 +20,26 @@ app.get('/books/:id',function(req,res){
         if(err) res.send(err);
         res.json(Book);
 });});
+app.post('/books',urlencoderparser,function(req,res){
+    var newBook=new Book();
+    newBook.title=req.body.title;
+    newBook.author=req.body.author;
+    newBook.category=req.body.category;
 
+    newBook.save(function(err,Book)
+    {
+        if(err) res.send(err);
+        console.log(Book);
+        res.json(Book);
+    });
+});
+
+app.post('/book2',function(req,res){
+    Book.create(req.body,function(err,Book){
+        if(err) res.send(err);
+        console.log(Book);
+        res.json(Book);
+    });
+});
 app.listen(3000);
 console.log("Listening");
